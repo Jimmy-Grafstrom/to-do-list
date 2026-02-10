90d898ba16d1980d21a0cba7506de221b30dea0f
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-02-10 20:40:06 +0100

    Add GroceryList validator and custom exceptions

---
+++ src/main/java/se/jimmy/iths/todolist/validator/GroceryListValidator.java
@@ -0,0 +1,37 @@
+package se.jimmy.iths.todolist.validator;
+
+import org.springframework.stereotype.Component;
+import se.jimmy.iths.todolist.exceptions.GroceryListValidationException;
+
+@Component
+public class GroceryListValidator {
+    public void validateName(String name) {
+        if (name == null || name.length() == 0) {
+            throw new GroceryListValidationException("Grocery list name cannot be empty");
+        }
+    }
+
+    public void validateQuantity(int quantity) {
+        if (quantity < 1 || quantity > 100) {
+            throw new GroceryListValidationException("Quantity must be between 1 and 100");
+        }
+    }
+
+    
+    public void validateCategory(String category) {
+        if (category == null || category.length() == 0) {
+            throw new GroceryListValidationException("Category cannot be empty");
+        }
+    }
+
+    public void validatePurchase(boolean purchase) {
+
+    }
+
+    // ska detta med eller inte?
+//    public void validate(GroceryList groceryList) {
+//        validateName(groceryList.getName());
+//        validateQuantity(groceryList.getQuantity());
+//        validateCategory(groceryList.getCategory());
+//    }
+}
