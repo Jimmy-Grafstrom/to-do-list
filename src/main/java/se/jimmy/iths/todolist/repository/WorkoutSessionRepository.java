fc97b2d1e46cf581c8c007358a49263f8adc572d
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-02-11 12:10:15 +0100

    remove

--- src/main/java/se/jimmy/iths/todolist/repository/WorkoutSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package se.jimmy.iths.todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se.jimmy.iths.todolist.model.WorkoutSession;
-
-@Repository
-public interface WorkoutSessionRepository extends JpaRepository<WorkoutSession, Long> {
-}
